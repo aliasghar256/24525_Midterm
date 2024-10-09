b57beb3a9192c818aa3a21e193dadab281f96282
Ali Asghar Hussain <alih11737@gmail.com>
2024-10-09 16:43:01 +0500

    I'm Loosing It

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
+}
+
+//Defining the Class for an individual Mission Post
+class missionEntry {
+  final String? mission_name;
+  final String? mission_id;
+  final List<String>? manufacturers;
+  final List<String>? payload_ids;
+  final String? wikipedia;
+  final String? website;
+  final String? twitter;
+  final String? description;
+
+  const missionEntry({
+    required this.mission_name,
+    required this.mission_id,
+    required this.manufacturers,
+    required this.payload_ids,
+    required this.wikipedia,
+    required this.website,
+    required this.twitter,
+    required this.description,
+  });
+  factory missionEntry.fromJson(Map<String, dynamic> json) {
+    return missionEntry(
+      mission_name: json['mission_name'],
+      mission_id: json['mission_id'],
+      manufacturers: json['manufacturers'].cast<String>(),
+      payload_ids: json['payload_ids'].cast<String>(),
+      wikipedia: json['wikipedia'],
+      website: json['website'],
+      twitter: json['twitter'],
+      description: json['description'],
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -36,85 +72,58 @@ class _MissionPageState extends State<MissionPage> {
   List<missionEntry> missionList = [];
   bool isLoading = true;
 
-  Future<List<missionEntry>> fetchMissions() async {
-    final response = await http.get(Uri.parse(
-        'https://api.spacexdata.com/v3/missions')); //API endpoint for SpaceX missions
+  Future<void> fetchMissions() async {
+    final response =
+        await http.get(Uri.parse('https://api.spacexdata.com/v3/missions'));
     if (response.statusCode == 200) {
       final jsonResponse = jsonDecode(utf8.decode(response.bodyBytes)) as List;
       setState(() {
         missionList = jsonResponse
-            .map((launch) => missionEntry.fromJson(launch))
+            .map((mission) => missionEntry.fromJson(mission))
             .toList();
         isLoading = false;
       });
-      return jsonResponse
-          .map((mission) => new missionEntry.fromJson(mission))
-          .toList();
     } else {
       throw Exception('Error Loading missions');
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(),
-      ),
+  Widget missionListWidget() {
+    return ListView.builder(
+      itemCount: missionList.length,
+      itemBuilder: (context, index) {
+        return Container(
+            margin: EdgeInsets.all(8),
+            child: MissionPost(missionentry: missionList[index]));
+      },
     );
   }
-}
 
-//Defining the Class for an individual Mission Post
-class missionEntry {
-  final String mission_name;
-  final String mission_id;
-  final List<String> manufacturers;
-  final List<String> payload_ids;
-  final String wikipedia;
-  final String website;
-  final String twitter;
-  final String description;
+  @override
+  void initState() {
+    super.initState();
+    fetchMissions();
+  }
 
-  const missionEntry({
-    required this.mission_name,
-    required this.mission_id,
-    required this.manufacturers,
-    required this.payload_ids,
-    required this.wikipedia,
-    required this.website,
-    required this.twitter,
-    required this.description,
-  });
-  factory missionEntry.fromJson(Map<String, dynamic> json) {
-    return missionEntry(
-      mission_name: json['mission_name'],
-      mission_id: json['mission_id'],
-      manufacturers: json['manufacturers'].cast<String>(),
-      payload_ids: json['payload_ids'].cast<String>(),
-      wikipedia: json['wikipedia'],
-      website: json['website'],
-      twitter: json['twitter'],
-      description: json['description'],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Space Missions'),
+        backgroundColor: Colors.green[900],
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : missionListWidget(),
     );
   }
 }
 
 //We make a stateful mission post as it's state can change depending upon the pressing of a button.
 class MissionPost extends StatefulWidget {
-  const MissionPost({super.key});
+  final missionEntry missionentry;
+  const MissionPost({super.key, required this.missionentry});
 
   @override
   State<MissionPost> createState() => _MissionPostState();
@@ -123,6 +132,19 @@ class MissionPost extends StatefulWidget {
 //We now assemble the composition of the mission post.
 //Since I need rounded corners and a shadow I use card
 class _MissionPostState extends State<MissionPost> {
+  bool expanded = false;
+  //Random Color Function
+  // Function to generate a random color
+  Color getRandomColor() {
+    final Random random = Random();
+    return Color.fromARGB(
+      255,
+      random.nextInt(256), // Red
+      random.nextInt(256), // Green
+      random.nextInt(256), // Blue
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -135,7 +157,7 @@ class _MissionPostState extends State<MissionPost> {
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text('${widget.missionEntry.missionName}',
+                Text('${widget.missionentry.mission_name}',
                     style: TextStyle(fontWeight: FontWeight.bold)),
               ],
             ),
@@ -147,7 +169,7 @@ class _MissionPostState extends State<MissionPost> {
               children: [
                 Flexible(
                   child: Text(
-                    "${widget.missionEntry.description}",
+                    "${widget.missionentry.description}",
                     overflow:
                         expanded ? TextOverflow.visible : TextOverflow.ellipsis,
                   ),
@@ -193,8 +215,8 @@ class _MissionPostState extends State<MissionPost> {
             ), // Spacing
 
             // Payload IDs Section (Wrap with pill-shaped containers)
-            if (widget.missionEntry.payloadIds != null &&
-                widget.missionEntry.payloadIds!.isNotEmpty)
+            if (widget.missionentry.payload_ids != null &&
+                widget.missionentry.payload_ids!.isNotEmpty)
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -205,7 +227,7 @@ class _MissionPostState extends State<MissionPost> {
                     child: Wrap(
                       spacing: 8.0, // Space between pills
                       runSpacing: 4.0, // Space between lines
-                      children: widget.missionEntry.payloadIds!.map((id) {
+                      children: widget.missionentry.payload_ids!.map((id) {
                         final color = getRandomColor(); // Get random color
                         return Container(
                           padding: const EdgeInsets.symmetric(
